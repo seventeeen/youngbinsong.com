891a15b0602917c0a8066393f00eb3876eb3f7e4
seventeen <powdragon1@gmail.com>
2018-10-27 13:17:15 +0900

    firebase setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
+import firebase from 'firebase/app'
+import VueFire from 'vuefire'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCp0i_KC75JLMBXL_JKDGA7b8YrmbiKNVA',
+  authDomain: 'youngbinsong-com.firebaseapp.com',
+  databaseURL: 'https://youngbinsong-com.firebaseio.com',
+  projectId: 'youngbinsong-com',
+  storageBucket: 'youngbinsong-com.appspot.com',
+  messagingSenderId: '911019205648'
+})
 
 new Vue({
   router,
