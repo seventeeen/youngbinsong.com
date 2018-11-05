a33b8db02f54a7ca445283a3f6cc085f82049efb
seventeen <powdragon1@gmail.com>
2018-11-06 05:15:23 +0900

    resource.json 추가
     * resource 사용 예시코드 작성
    
    Signed-off-by: seventeen <powdragon1@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img :src="logo"/>
     <HelloWorld msg="Welcome to Your Vue.js App, 한글폰트"/>
   </div>
 </template>
@@ -8,11 +8,23 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import resource from '@/resource.json'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      logo: resource.img.logo
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  img {
+    width: 200px;
+  }
+</style>
