7087d53ca35934fb3db25eeb93b97c64240cf33f
seventeen <powdragon1@gmail.com>
2018-11-06 21:54:18 +0900

    미사용 코드 제거
    Signed-off-by: seventeen <powdragon1@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,16 @@
 <template>
   <div class="home">
-    <img :src="logo"/>
     <HelloWorld msg="Welcome to Your Vue.js App, 한글폰트"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import resource from '@/resource.json'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
-  },
-  data () {
-    return {
-      logo: resource.img.logo
-    }
   }
 }
 </script>
